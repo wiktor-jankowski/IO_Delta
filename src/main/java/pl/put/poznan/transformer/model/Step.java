67460382f02b560fb549fb267f8bff9b889ddeb9
Rakeduk <69343181+Rakeduk@users.noreply.github.com>
2021-12-16 14:05:55 +0100

    Adding getters and setter of Steps

--- src/main/java/pl/put/poznan/transformer/model/Step.java
+++ src/main/java/pl/put/poznan/transformer/model/Step.java
@@ -14,4 +14,42 @@ public class Step {
 
     public Step() { }
 
+
+
+    // getters and setters section
+    public Integer getStepNum() {
+        return numberOfStep;
+    }
+
+    public void setStepNum(Integer stepNum) {
+        this.numberOfStep = stepNum;
+    }
+
+    public String getActor() { return actor; }
+
+    public void setActor(String actor) { this.actor = actor; }
+
+    public List<String> getContent() {
+        return rule;
+    }
+
+    public void setContent(List<String> content) {
+        this.rule = content;
+    }
+
+    public List<Step> getSubsteps() {
+        return subStep;
+    }
+
+    public void setSubsteps(List<Step> substeps) {
+        this.subStep = substeps;
+    }
+
+    public List<Integer> getReturnPath() {
+        return deepPath;
+    }
+
+    public void setReturnPath(List<Integer> returnPath) {
+        this.deepPath = returnPath;
+    }
 }
