fbdd387ef62d5b5a1f455270c4c2c7f053d2a406
Rakeduk <69343181+Rakeduk@users.noreply.github.com>
2021-12-19 10:43:30 +0100

    Update ScenarioModel.java
    Correct Getters and setters

--- src/main/java/pl/put/poznan/scenarioQualityChecker/model/ScenarioModel.java
+++ src/main/java/pl/put/poznan/scenarioQualityChecker/model/ScenarioModel.java
@@ -1,9 +1,9 @@
 package pl.put.poznan.scenarioQualityChecker.model;
 
 import java.util.ArrayList;
-import java.util.List;
 import pl.put.poznan.scenarioQualityChecker.logic.VisitorForCounting;
-
+import pl.put.poznan.scenarioQualityChecker.logic.VisitorForDisplaying;
+import pl.put.poznan.scenarioQualityChecker.logic.DisplayingScenario;
 
 
 public class ScenarioModel {
@@ -19,6 +19,16 @@ public class ScenarioModel {
         for(Step step : steps)
         {
             step.acceptCounting(visitor);
+        }
+    }
+
+    public void acceptDisplaying(VisitorForDisplaying visitor)
+    {
+        if(visitor instanceof DisplayingScenario)
+            ((DisplayingScenario)visitor).setScenarioText(this.titleOfScenario + "</br></br>");
+        for(Step step : steps)
+        {
+            step.acceptDisplaying(visitor, "", 0);
         }
     }
 
